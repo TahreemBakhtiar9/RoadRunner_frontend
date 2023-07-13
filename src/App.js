ecda7d7815125f56e025da84f08b0e7f3196b8ed
TahreemBakhtiar9 <tehreembakhtiar927@gmail.com>
2023-07-13 16:27:24 +0500

    done userInfo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { CarDesc } from './pages/CarDesc';
 import {UserInfo} from './pages/UserInfo'
+import { Checkout } from './pages/Checkout';
 
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path={'car/:id'} element={<CarDesc/>}/>
-          <Route path='userinfo' element={<UserInfo/>}/>
+          <Route path='/userinfo' element={<UserInfo/>}/>
+          <Route path='/checkout' element={<Checkout/>}/>
         </Routes>
       </Router>
       
